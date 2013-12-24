60a209815b06546879dd1d0db16294b2bc21a458
Yuval Ziegler <YuvalZiegler@gmail.com>
2013-12-23 21:17:15 -0500

    debug window toggle

--- themes/kiehls_mw/js/site.js
+++ themes/kiehls_mw/js/site.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function($) {
 
+  $(document).on('click', "[data-toggle-target-id]", function(){
 
+    var targetID = $(this).data().toggleTargetId;
+    $(targetID).fadeToggle();
+  })
 
 });
